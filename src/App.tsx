ed31c95b355e5eec1eeffaad31bdeef790a673db
Iamtheusername112 <apkfem@gmail.com>
2023-08-15 13:11:39 +0200

    working on the ProfilePics components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import ProfilePics from "./components/ProfilePics";
 import SideBar from "./components/SideBar";
 
 function App() {
@@ -7,6 +8,13 @@ function App() {
     <>
       <Header />
       <SideBar />
+      <div className="ml-[90px] grid grid-cols-1 md:grid-cols-3">
+        <div className="col-span-2">User Data</div>
+
+        <div className="hidden md:block">
+          <ProfilePics />
+        </div>
+      </div>
     </>
   );
 }
